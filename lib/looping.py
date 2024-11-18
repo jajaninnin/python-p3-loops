82808d4eecddbb29b2a9fca5bfbf176cc882954b
janine pamintuan <jaja.ninnin@gmail.com>
2024-11-18 13:02:27 -0500

    python loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    num = 10
+    while True:
+        print(num)
+        num = num - 1
+        if num == 0:
+            break
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_list = []
+    for item in int_list:
+        square = item ** 2
+        square_list.append(square)
+    return square_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    num = 1
+    while True:
+        if (num % 3 == 0) and (num % 5 ==0):
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+        num += 1
+        if num > 100:
+            break
+
+fizzbuzz()
